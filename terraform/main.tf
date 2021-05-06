b93e3bd382d396ca00401fbcaf81ab8538412c9d
Leon Jalfon <leonjalfon1@gmail.com>
2021-05-06 03:45:38 +0300

    add storageaccount

--- terraform/main.tf
+++ terraform/main.tf
@@ -66,3 +66,16 @@ resource "azurerm_linux_virtual_machine" "myvm" {
     version   = "latest"
   }
 }
+
+# Create a storage accout
+resource "azurerm_storage_account" "storageaccount" {
+  name                     = "leonjalfonexampletf"
+  resource_group_name      = azurerm_resource_group.myresourcegroup.name
+  location                 = azurerm_resource_group.myresourcegroup.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
