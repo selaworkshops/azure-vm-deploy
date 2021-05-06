c5b07decdb24a23c525f41593be01636e43ec84d
selaworkshops <39066255+selaworkshops@users.noreply.github.com>
2021-05-06 12:59:07 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -66,16 +66,3 @@ resource "azurerm_linux_virtual_machine" "myvm" {
     version   = "latest"
   }
 }
-
-# Create a storage accout
-resource "azurerm_storage_account" "storageaccount" {
-  name                     = "leonjalfonexampletftest"
-  resource_group_name      = azurerm_resource_group.myresourcegroup.name
-  location                 = azurerm_resource_group.myresourcegroup.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-
-  tags = {
-    environment = "staging"
-  }
-}
